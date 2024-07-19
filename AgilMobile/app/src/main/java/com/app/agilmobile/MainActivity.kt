9a6b2c168d50af39d28c364cc0e0ab83c196b57d
CarlosGoes0 <carlos.goes@sptech.school>
2024-07-19 17:11:40 -0300

    Permissões solicitadas em tempo de execução funcional

--- AgilMobile/app/src/main/java/com/app/agilmobile/MainActivity.kt
+++ AgilMobile/app/src/main/java/com/app/agilmobile/MainActivity.kt
@@ -1,33 +1,40 @@
 package com.app.agilmobile
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
-import android.text.Layout.Alignment
 import android.view.View
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat
 import androidx.core.view.WindowCompat
 import com.app.agilmobile.ui.routes.AppNavigation
-import com.app.agilmobile.ui.screens.LoginScreen
-import com.app.agilmobile.ui.screens.MainScreen
 import com.app.agilmobile.ui.theme.AgilMobileTheme
 
-
-
 class MainActivity : ComponentActivity() {
+
+    private val permissions = arrayOf(
+        Manifest.permission.CAMERA,
+        Manifest.permission.RECORD_AUDIO,
+        Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.ACCESS_COARSE_LOCATION
+    )
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+            permissions.entries.forEach {
+                // Processar resultado de permissões se necessário
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Configurações de tela cheia
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         window.decorView.systemUiVisibility = (
                 View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                         or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
@@ -35,10 +42,24 @@ class MainActivity : ComponentActivity() {
                         or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                 )
         enableEdgeToEdge()
+
+        // Verificar e solicitar permissões
+        if (!arePermissionsGranted()) {
+            requestPermissionLauncher.launch(permissions)
+        }
+
         setContent {
             AgilMobileTheme {
                 AppNavigation()
             }
+        }
+    }
+
+    private fun arePermissionsGranted(): Boolean {
+        return permissions.all {
+            ContextCompat.checkSelfPermission(
+                this, it
+            ) == PackageManager.PERMISSION_GRANTED
         }
     }
 }
